3d66b14241a6a417236231bd39aab51c3377278a
Mattias Mohlin <mattias.mohlin@hcl.com>
2024-03-08 09:54:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# gRPC Sample
+[gRPC](https://grpc.io/) is a Remote Procedure Call (RPC) framework for building systems of distributed applications in heterogeneous environments. This sample shows how you can use it in applications created with [DevOps Model RealTime](https://www.hcl-software.com/devops-model-realtime).
+
+This sample contains a [gRPC server](/blob/main/grpc-server) (implemented with Model RealTime) and a [gRPC client]((/blob/main/grpc-client)) (implemented as a stand-alone C++ command-line application).
+
+## Preparations
+1. Download, build and install gRPC
+
+Follow the steps in the [gRPC QuickStart guide](https://grpc.io/docs/languages/cpp/quickstart/) to install required build tools, clone the [gRPC GitHub repo](https://github.com/grpc/grpc), build gRPC from its source code and install the gRPC tools and libraries. Windows users can find more detailed instructions [here](https://github.com/grpc/grpc/blob/v1.61.0/BUILDING.md).
+
+**NOTE 1:** The sample is by default configured to be built with Visual Studio 17 (2022) using cmake for Win64 Debug. If you want to use a different compiler, target platform or build configuration you must adjust arguments to cmake when building gRPC. You then also must update the TC in the server project and use an appropriate cmake generator for building the client accordingly. The further steps assume you have the correct build tools in the path (for Visual Studio this is best accomplished by using a Visual Studio command prompt).
+
+**NOTE 2:** If you go with the default build configuration you should also build the TargetRTS with the same settings. Follow [these steps](https://model-realtime.hcldoc.com/help/topic/com.ibm.xtools.rsarte.webdoc/Articles/Running%20and%20debugging/Debugging%20the%20RT%20services%20library.html?cp=23_2_13_1). In addition set the `/MDd` flag in `LIBSETCCFLAGS` when you edit `libset.mk` before building it.
+ 
+2. Add the location where you installed the gRPC tools to your PATH environment variable
+
+## Build the client
+3. You need to build the client before the server since it contains the [.proto file](/blob/main/grpc-client/proto/maze.proto) that describes the RPCs implemented by the sample. This file is used both by the client and the server.
+
+```
+..grpc-client/build> cmake .. -G "Visual Studio 17 2022"
+..grpc-client/build> cmake --build . --config Debug
+```
+
+This will generate a Visual Studio solution file (`MazeWalker.sln`) which you can use for debugging the client application.
+
+## Build the server
+4. Open the [gRPC server](/blob/main/grpc-server) project in Model RealTime.
+5. Open the TC `server.tcjs` and edit the variables in the beginning of the file that specify the location where you placed the gRPC source code (`grpcSourceLocation`) and where you installed the gRPC tools and libraries (`grpcInstallLocation`).
+6. Also edit the `targetServicesLibrary` property to specify the path to the TargetRTS to use. If you want to use the default TargetRTS that comes with Model RealTime, you can delete this property, but remember that with that version you cannot debug. Other properties that have to be modified depending on if you want to debug or not (and which compiler that is used) are `compileArguments` and `linkArguments`. By default they are set so you can debug with Visual Studio.
+7. Build the TC.
+
+## Run the sample
+The sample application is about walking a maze represented with a state machine in the `Maze` capsule. From a state in the maze you can either go east, west, north or south. Valid paths are represented by transitions. If you find your way out of the maze (not very hard) you reach the goal.
+
+![](images/maze.png)
+
+The capsule has a `stepCount` variable which keeps track of the number of steps taken in the maze.
+
+Launch the server executable that was generated when building the TC. If you launch with the Model Debugger you can visually inspect how the active state changes in the maze, and how the `stepCount` variable gets updated, as a result of client requests.
+
+Finally launch the client executable `maze_client.exe` that was built previously. It has a simple command prompt where you can send commands to the server for walking the maze:
+
+* **east, west, north, south** Take a step in the direction (if possible)
+* **steps** Report the number of steps taken so far
+* **adjust <arg>** Adjust the step counter with the specified argument (an integer)
+* **subscribe** Subscribe to get notified about an attempt to take a step in a direction that is not possible.
+* **unsubscribe** Unsubscribe from the notifications about taking the wrong way in the maze.
+* **exit** Terminate the client
+
+**NOTE** The client and server communicate on port 50051 on localhost (i.e. you must run them on the same machine). You can change the port and hostname by means of the `--target` command-line argument for the client. However, the same is currently hardcoded in the server (set the default value of `GRPC_Server::port`).
+
+For example, to connect to the server running on a different machine and on a different port:
+```
+maze_client.exe --target 172.27.223.1:50052
+```
+
+## How the sample works
+Refer to the [.proto file](/blob/main/grpc-client/proto/maze.proto) and the picture below to understand how the sample works:
+
+![](images/server_client.png)
+
+### Asynchronous request without data
+The "west", "east", "north" and "south" commands will cause an event to be asynchronously sent. They don't have a data parameter.
+
+```proto
+// Send an event without data
+  rpc GoWest (google.protobuf.Empty) returns (google.protobuf.Empty) {}
+  rpc GoEast (google.protobuf.Empty) returns (google.protobuf.Empty) {}
+  rpc GoNorth (google.protobuf.Empty) returns (google.protobuf.Empty) {}
+  rpc GoSouth (google.protobuf.Empty) returns (google.protobuf.Empty) {}
+```
+
+When the client sends these requests they will be intercepted by the server's thread (`ServerJob`) which will transfer them to the `gRPC_Server` capsule through its `external` port. Each request is represented by a subclass of the `CallData` class which implements a simple state machine describing the life cycle of an RPC request. The `gRPC_Server` capsule, which is run by the server's main thread, will then perform the sending of the corresponding event from the `Commands` protocol (`west()`, `east()`, `north()` and `south` respectively). This happens in  `CallData_Go::completeRequest()`. Note that since these events are asynchronously sent, the client can immediately proceed its execution and doesn't need to wait for a reply.
+
+### Asynchronous request with data
+The "adjust" command takes an integer as argument and the corresponding RPC therefore has an input message for transfering that data.
+
+```proto
+  // Send an event with data
+  rpc AdjustStepCount (AdjustStepCountRequest) returns (google.protobuf.Empty) {}
+```
+
+This request is handled by the server in exactly the same way as described above. The only difference is that the data that is passed is used as data for the parameter of the `adjustStepCount()` event. This happens in  `CallData_AdjustStepCount::completeRequest()`. 
+
+### Synchronous request with reply data
+The "steps" command causes the client to make a synchronous request to the server for getting the current step count. 
+
+```proto
+  // Invoke an event with reply data
+  rpc StepCount (google.protobuf.Empty) returns (StepCountReply) {}
+```
+
+The server handles this request in the same way as the asynchronous requests, but the client will wait until the server finishes the request and provides the reply data (which is a simple integer). This happens in  `CallData_StepCount::completeRequest()`. 
+
+### Subscribing for outgoing events
+The client can subscribe to get notified by the server when an outgoing event is received by the `gRPC_Server` capsule on its `commands` port. 
+
+```proto
+  // Subscribe for outgoing event with data
+  rpc Subscribe_WrongWay (google.protobuf.Empty) returns (stream WrongWay) {}
+  rpc Subscribe_GoalReached (google.protobuf.Empty) returns (stream StepCountReply) {} // Messages can be "reused"
+  ```
+
+  Subscription requests are managed by the server in a slightly different way than the requests covered so far. A subclass of `SubscribeData` is used for representing a subscription request. When it reaches the `gRPC_Server` capsule it's not immediately completed like other requests. Instead it is inserted into a map with an id string as key. The sample uses the name of the event ("GoalReached", "WrongWay") as the id string, but any id that uniquely identifies the event that is subscribed for can be used.
+
+  Internal transitions in the `WaitForRequest` state of the `MyGRPC_Server` capsule get triggered when outgoing events arrive. They call `GRPC_Server::getSubscription()` for checking if there currently is an active subscription for the received event. If so, `SubscribeData::notifySubscriber()` is called on the subscription request object. This is where the server notifies the client about the received outgoing event by writing a reply for the subscription request.
+
+  Note that the client needs to launch a separate thread which can wait for notifications from active subscriptions, without blocking the main thread. In the sample, the client uses one such thread for each outgoing event it subscribes to (at most two), but other alternatives, such as using a single thread for managing notifications from all active subscriptions, would also be possible.
+
+### Unsubscribing for outgoing events
+The server may offer a way for the client to unsubscribe from getting notified for outgoing events. In the sample, the server offers this for the `wrongWay()` event.
+
+```proto
+// Unsubscribe for outgoing event
+  rpc Unsubscribe_WrongWay (google.protobuf.Empty) returns (google.protobuf.Empty) {}
+```
+
+This is a regular request which is handled exactly like other non-subscription requests. From `CallData_Unsubscribe_WrongWay::completeRequest()` the subscription request is removed from the `gRPC_Server` capsule's map of active subscriptions by a call to `GRPC_Server::unsubscribe()`. Then the subscription request is finished, see `SubscribeData_WrongWay_unsubscribe()`.
+
+In the sample client, the finishing of a subscription request will terminate the subscription thread.
---
+++ grpc-client/src/maze_client.cpp
@@ -0,0 +1,286 @@
+#include <iostream>
+#include <thread>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+
+#include <grpcpp/grpcpp.h>
+
+
+#include "maze.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using grpc::ServerCompletionQueue;
+using maze::MazeWalker;
+using maze::StepCountReply;
+using maze::AdjustStepCountRequest;
+using maze::WrongWay;
+
+
+ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
+
+class MazeClient {
+public:
+    MazeClient(std::shared_ptr<Channel> channel)
+        : stub_(MazeWalker::NewStub(channel)) {}
+
+    enum Direction {
+        East, West, North, South
+    };
+
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    void GoDirection(Direction direction) {
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        ::google::protobuf::Empty emptyRequest;
+
+        // The actual RPC.
+        Status status;
+        if (direction == East) 
+            status = stub_->GoEast(&context, emptyRequest, &emptyRequest);
+        else if (direction == West)
+            status = stub_->GoWest(&context, emptyRequest, &emptyRequest);
+        else if (direction == North)
+            status = stub_->GoNorth(&context, emptyRequest, &emptyRequest);
+        else if (direction == South)
+            status = stub_->GoSouth(&context, emptyRequest, &emptyRequest);
+
+        if (!status.ok())
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }
+
+    int32_t StepCount() {
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        ::google::protobuf::Empty emptyRequest;
+
+        // Container for the data we expect from the server.
+        StepCountReply reply;
+
+        // The actual RPC.
+        Status status = stub_->StepCount(&context, emptyRequest, &reply);
+
+        if (!status.ok()) {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            return -1;
+        }
+
+        return reply.count();
+    }
+
+    void AdjustStepCount(int32_t adjustment) {
+        ClientContext context;
+        AdjustStepCountRequest request;
+        request.set_adjustment(adjustment);
+
+        ::google::protobuf::Empty emptyRequest;
+
+        // The actual RPC.
+        Status status = stub_->AdjustStepCount(&context, request, &emptyRequest);
+        if (!status.ok())
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }    
+
+    // Called by reader thread!
+    void subscribeForWrongWay() {
+
+        class Reader : public grpc::ClientReadReactor<WrongWay> {
+        public:
+            Reader(MazeWalker::Stub* stub) {
+                ::google::protobuf::Empty emptyRequest;
+
+                stub->async()->Subscribe_WrongWay(&context_, &emptyRequest, this);
+                StartRead(&ww_);
+                StartCall();
+            }
+            void OnReadDone(bool ok) override {
+                if (ok) {                    
+                    const std::string str = ww_.message();
+                    std::cout << "WrongWay: " << str << std::endl;
+                    std::cout << ">" << std::flush; // Repeat prompt
+
+                    // Read next
+                    StartRead(&ww_);
+                }
+            }
+            void OnDone(const Status& s) override {
+                std::unique_lock<std::mutex> l(mu_);
+                status_ = s;
+                done_ = true;
+                cv_.notify_one();
+            }
+            Status Await() {
+                std::unique_lock<std::mutex> l(mu_);
+                cv_.wait(l, [this] { return done_; });
+                return std::move(status_);
+            }
+
+            WrongWay ww_;
+
+        private:
+            ClientContext context_;
+            
+            std::mutex mu_;
+            std::condition_variable cv_;
+            Status status_;
+            bool done_ = false;
+        };
+    
+        Reader reader(stub_.get());
+        // Block reader thread while the subscribe RPC is active (which it will be until we unsubscribe)
+        Status status = reader.Await();
+        if (!status.ok()) {            
+            std::cout << "Subscribe_WrongWay rpc failed." << std::endl;
+        }    
+        subscribed = false;
+        // Reader thread terminates when we return from here
+    }
+
+    // Called by reader thread!
+    void subscribeForGoalReached() {
+
+        class Reader : public grpc::ClientReadReactor<StepCountReply> {
+        public:
+            Reader(MazeWalker::Stub* stub) {
+                ::google::protobuf::Empty emptyRequest;
+
+                stub->async()->Subscribe_GoalReached(&context_, &emptyRequest, this);
+                StartRead(&scr_);
+                StartCall();
+            }
+            void OnReadDone(bool ok) override {
+                if (ok) {
+                    const unsigned int stepCount = scr_.count();
+                    std::cout << "The goal was reached in " << stepCount << " steps!" << std::endl;
+                    std::cout << ">" << std::flush; // Repeat prompt
+
+                    // Read next
+                    StartRead(&scr_);
+                }
+            }
+            void OnDone(const Status& s) override {
+                std::unique_lock<std::mutex> l(mu_);
+                status_ = s;
+                done_ = true;
+                cv_.notify_one();
+            }
+            Status Await() {
+                std::unique_lock<std::mutex> l(mu_);
+                cv_.wait(l, [this] { return done_; });
+                return std::move(status_);
+            }
+
+            StepCountReply scr_;
+
+        private:
+            ClientContext context_;
+
+            std::mutex mu_;
+            std::condition_variable cv_;
+            Status status_;
+            bool done_ = false;
+        };
+
+        Reader reader(stub_.get());
+        // Block reader thread while the subscribe RPC is active (which it will be until we unsubscribe)
+        Status status = reader.Await();
+        // We will never get here, since the client never unsubscribes for the GoalReached event
+        if (!status.ok()) {
+            std::cout << "Subscribe_GoalReached rpc failed." << std::endl;
+        }
+    }
+
+    void Unsubscribe_WrongWay() {
+        ClientContext context;
+
+        ::google::protobuf::Empty emptyRequest;
+
+        Status status = stub_->Unsubscribe_WrongWay(&context, emptyRequest, &emptyRequest);
+
+        if (!status.ok())
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    }
+
+    std::atomic<bool> subscribed = false;
+
+private:
+    std::unique_ptr<MazeWalker::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+    absl::ParseCommandLine(argc, argv);
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint specified by
+    // the argument "--target=" which is the only expected argument.
+    std::string target_str = absl::GetFlag(FLAGS_target);
+    // We indicate that the channel isn't authenticated (use of
+    // InsecureChannelCredentials()).
+    MazeClient mazeWalker(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+
+    // Launch reader thread to receive notifications when the GoalReached events arrives.
+    // Note that we never unsubscribe for this event, so this thread will run until the application terminates
+    std::thread subscribeForGoalReached(&MazeClient::subscribeForGoalReached, &mazeWalker);
+    subscribeForGoalReached.detach();
+
+    std::cout << "Walk the maze. Commands: " << std::endl;
+    std::cout << "east, west, north, south : Take a step in a direction" << std::endl;
+    std::cout << "steps : Report number of steps taken" << std::endl;
+    std::cout << "adjust : Adjust step count by specified number" << std::endl;
+    std::cout << "subscribe : Subscribe to be notified when wrong way taken" << std::endl;
+    std::cout << "unsubscribe : Unsubscribe to be notified when wrong way taken" << std::endl;
+    std::cout << "exit : Exit" << std::endl;
+    std::string command;
+    while (true) {
+        std::cout << ">" << std::flush;
+        std::getline(std::cin, command);
+        if (command == "east")
+            mazeWalker.GoDirection(MazeClient::East);
+        else if (command == "west")
+            mazeWalker.GoDirection(MazeClient::West);
+        else if (command == "north")
+            mazeWalker.GoDirection(MazeClient::North);
+        else if (command == "south")
+            mazeWalker.GoDirection(MazeClient::South);
+        else if (command == "steps") {
+            int32_t count = mazeWalker.StepCount();
+            std::cout << "Step count: " << count << std::endl;
+        }
+        else if (command.rfind("adjust", 0) == 0) {
+            std::string a = command.substr(command.find(" ") + 1);
+            int32_t adjustment = std::stoi(a);
+            mazeWalker.AdjustStepCount(adjustment);
+        }
+        else if (command.rfind("subscribe", 0) == 0) {
+            if (mazeWalker.subscribed) {
+                std::cout << "You are already subscribed!" << std::endl;
+                continue;
+            }
+
+            mazeWalker.subscribed = true;
+
+            // Launch reader thread to receive notifications when the WrongWay event arrives
+            std::thread subscribeForWrongWayThread(&MazeClient::subscribeForWrongWay, &mazeWalker);
+            subscribeForWrongWayThread.detach();
+        }
+        else if (command.rfind("unsubscribe", 0) == 0) {
+            if (!mazeWalker.subscribed) {
+                std::cout << "You are already unsubscribed!" << std::endl;
+                continue;
+            }
+
+            mazeWalker.Unsubscribe_WrongWay();
+        }
+        else if (command == "exit")
+            return 0;
+        else {
+            std::cout << "Unknown command '" << command << "'" << std::endl;
+        }
+    }
+}
